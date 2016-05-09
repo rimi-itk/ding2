3fb67adaa0944feedb55005824d7c031ddde2e97
Mikkel Ricky <rimi@aarhus.dk>
2016-05-09 14:55:35 +0200

    Issue #BPI-89 by rimi-itk: Cleaned up and improved code and templates

--- modules/bpi/templates/channel-view.tpl.php
+++ modules/bpi/templates/channel-view.tpl.php
@@ -9,12 +9,18 @@
 	<div class="description"><?php echo $channel->getDescription(); ?></div>
 
 	<div>
-		<?php echo t('Administrator'); ?>: <?php echo l($channel->getAdmin()->getName(), 'admin/bpi/user/' . $channel->getAdmin()->getId()); ?>
+		<?php echo t('Channel administrator'); ?>: <?php echo l($channel->getAdmin()->getName(), 'admin/bpi/user/' . $channel->getAdmin()->getId()); ?>
 	</div>
 
-	<h2><?php echo t('Editors'); ?></h2>
+	<h2><?php echo t('Channel editors'); ?></h2>
 
-	<p><?php if (bpi_can_edit_channel($channel)) { echo l(t('Add editors'), 'admin/bpi/user', array('query' => array('add-to-channel' => $channel->getId()))); } ?></p>
+	<?php if (bpi_can_edit_channel($channel)): ?>
+		<ul class="action-links">
+			<li>
+				<?php echo l(t('Add editors to channel'), 'admin/bpi/user', array('query' => array('add-to-channel' => $channel->getId()))); ?>
+			</li>
+		</ul>
+	<?php endif ?>
 
 	<?php echo theme('bpi_user_list', array(
     'items' => $channel->getEditors(),
@@ -22,39 +28,19 @@
     'empty' => t('Channel has no editors'),
   )); ?>
 
-	<h2><?php echo t('Content'); ?></h2>
+	<h2><?php echo t('Channel content'); ?></h2>
 
-	<p><?php if (bpi_can_contribute_to_channel($channel)) { echo l(t('Add nodes'), 'admin/bpi/'); } ?></p>
-
-	<?php if ($nodes): ?>
-		<table>
-			<?php foreach ($nodes as $node): $properties = $node->getProperties(); ?>
-				<tr>
-					<td><?php echo $properties['id']; ?></td>
-					<td><?php echo l(t('Preview'), 'admin/bpi/preview/' . 'nojs' . '/' . $properties['id']); ?></td>
-					<td><?php echo l(t('Syndicate'), 'admin/bpi/syndicate/' . $properties['id']); ?></td>
-					<td><?php echo l(t('Remove'), 'admin/bpi/channel/' . $channel->getId() . '/node/remove/' . $properties['id']); ?></td>
-				</tr>
-			<?php endforeach ?>
-		</table>
-	<?php elseif ($channel->getNodes()): ?>
-		<table>
-			<?php foreach (array_map(function($id) {
-				return (object)[
-					'id' => $id,
-				];
-			}, $channel->getNodes()) as $node): ?>
-				<tr>
-					<td><?php echo $node->id; ?></td>
-					<td>
-						<?php echo l(t('Preview'), 'admin/bpi/preview/' . 'nojs' . '/' . $node->id); ?>
-						<?php echo l(t('Syndicate'), 'admin/bpi/syndicate/' . $node->id); ?>
-						<?php echo l(t('Remove'), 'admin/bpi/channel/' . $channel->getId() . '/node/remove/' . $node->id); ?>
-					</td>
-				</tr>
-			<?php endforeach ?>
-		</table>
-	<?php else: ?>
-		<p><?php echo t('No nodes in channel'); ?></p>
+	<?php if (bpi_can_contribute_to_channel($channel)): ?>
+		<ul class="action-links">
+			<li>
+				<?php echo l(t('Add nodes to channel'), 'admin/bpi/'); ?>
+			</li>
+		</ul>
 	<?php endif ?>
+
+	<?php echo theme('bpi_channel_node_list', array(
+    'items' => $nodes,
+    '#channel' => $channel,
+    'empty' => t('Channel has no nodes'),
+  )); ?>
 </div>
