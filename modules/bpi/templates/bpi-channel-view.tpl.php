821e86f9bbc3645883e100db7e4ac568744ab7d8
Mikkel Ricky <rimi@aarhus.dk>
2016-10-14 13:25:25 +0200

    Cleaned up code to match Drupal code style

--- modules/bpi/templates/bpi-channel-view.tpl.php
+++ modules/bpi/templates/bpi-channel-view.tpl.php
@@ -1,42 +1,42 @@
 <div class="channel-view">
-	<div class="bpi-actions">
-		<?php if (bpi_can_edit_channel($channel)) { echo l(t('Edit'), 'admin/bpi/channel/' . $channel->getId() . '/edit'); } ?>
-		<?php if (bpi_can_edit_channel($channel)) { echo l(t('Delete'), 'admin/bpi/channel/' . $channel->getId() . '/delete'); } ?>
-	</div>
+  <div class="bpi-actions">
+    <?php if (bpi_can_edit_channel($channel)) { echo l(t('Edit'), 'admin/bpi/channel/' . $channel->getId() . '/edit'); } ?>
+    <?php if (bpi_can_edit_channel($channel)) { echo l(t('Delete'), 'admin/bpi/channel/' . $channel->getId() . '/delete'); } ?>
+  </div>
 
-	<h1><?php echo $channel->getName(); ?></h1>
+  <h1><?php echo $channel->getName(); ?></h1>
 
-	<div class="description"><?php echo $channel->getDescription(); ?></div>
+  <div class="description"><?php echo $channel->getDescription(); ?></div>
 
-	<div>
-		<?php echo t('Channel administrator'); ?>: <?php echo l($channel->getAdmin()->getName(), 'admin/bpi/user/' . $channel->getAdmin()->getId()); ?>, <?php echo $channel->getAdmin()->getAgency()->getName(); ?>
-	</div>
+  <div>
+    <?php echo t('Channel administrator'); ?>: <?php echo l($channel->getAdmin()->getName(), 'admin/bpi/user/' . $channel->getAdmin()->getId()); ?>, <?php echo $channel->getAdmin()->getAgency()->getName(); ?>
+  </div>
 
-	<h2><?php echo t('Channel editors'); ?></h2>
+  <h2><?php echo t('Channel editors'); ?></h2>
 
-	<?php if (bpi_can_edit_channel($channel)): ?>
-		<ul class="action-links">
-			<li>
-				<?php echo l(t('Add editors to channel'), 'admin/bpi/user', array('query' => array('add-to-channel' => $channel->getId()))); ?>
-			</li>
-		</ul>
-	<?php endif ?>
+  <?php if (bpi_can_edit_channel($channel)): ?>
+    <ul class="action-links">
+      <li>
+<?php echo l(t('Add editors to channel'), 'admin/bpi/user', array('query' => array('add-to-channel' => $channel->getId()))); ?>
+      </li>
+    </ul>
+  <?php endif ?>
 
-	<?php echo theme('bpi_user_list', array(
+  <?php echo theme('bpi_user_list', array(
     'items' => $channel->getEditors(),
     '#channel' => $channel,
     'empty' => t('Channel has no editors'),
   )); ?>
 
-	<h2><?php echo t('Channel content'); ?></h2>
+  <h2><?php echo t('Channel content'); ?></h2>
 
-	<?php if (bpi_can_contribute_to_channel($channel)): ?>
-		<ul class="action-links">
-			<li>
-				<?php echo l(t('Add nodes to channel'), 'admin/bpi/'); ?>
-			</li>
-		</ul>
-	<?php endif ?>
+  <?php if (bpi_can_contribute_to_channel($channel)): ?>
+    <ul class="action-links">
+      <li>
+        <?php echo l(t('Add nodes to channel'), 'admin/bpi/'); ?>
+      </li>
+    </ul>
+  <?php endif ?>
 
   <?php if ($channel->getNodeLastAddedAt()): ?>
     <p>
@@ -46,11 +46,11 @@
 
   <?php if ($has_new_content): ?>
     <div class="messages status">
-			<?php echo t('Channel has new content!'); ?>
-		</div>
+      <?php echo t('Channel has new content!'); ?>
+    </div>
   <?php endif ?>
 
-	<?php echo theme('bpi_channel_node_list', array(
+  <?php echo theme('bpi_channel_node_list', array(
     'items' => $nodes,
     '#channel' => $channel,
     'empty' => t('Channel has no nodes'),
